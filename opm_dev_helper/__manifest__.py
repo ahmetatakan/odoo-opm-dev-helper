335af5c5b2d351fb230384b9caee54e8d7fafdb3
Ahmet Atakan <a.atakan@sachane.com.tr>
2025-10-21 08:59:05 +0300

    INIT 16.0

--- opm_dev_helper/__manifest__.py
+++ opm_dev_helper/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "OPM Dev Helper",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "summary": "Hot-reload for faster Odoo development (requires Odoo Plugin Manager CLI).",
     "description": """
     # OPM Dev Helper

61afa01dd59c5bc7484d9c7d298b4f9c4a2dd9b3
Ahmet Atakan <a.atakan@sachane.com.tr>
2025-10-24 14:07:49 +0300

    502 reload fixed

--- opm_dev_helper/__manifest__.py
+++ opm_dev_helper/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "OPM Dev Helper",
-    "version": "16.0.1.0.2",
+    "version": "16.0.1.2.0",
     "summary": "Hot-reload for faster Odoo development (requires Odoo Plugin Manager CLI).",
     "description": """
     # OPM Dev Helper

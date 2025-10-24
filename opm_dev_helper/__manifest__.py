c42f367cd493393b2c7d1cbc2edfbb85fb4eb478
Ahmet Atakan <a.atakan@sachane.com.tr>
2025-10-24 16:02:21 +0300

    feat: Odoo 19 compatibility

--- opm_dev_helper/__manifest__.py
+++ opm_dev_helper/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "OPM Dev Helper",
-    "version": "18.0.1.2.0",
+    "version": "19.0.1.0.0",
     "summary": "Hot-reload for faster Odoo development (requires Odoo Plugin Manager CLI).",
     "description": """
     # OPM Dev Helper
